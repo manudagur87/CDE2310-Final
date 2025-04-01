93221554f31745c75757ab201625fb02f90043f0
Manu <64531792+manudagur87@users.noreply.github.com>
2025-04-01 19:54:38 +0800

    Add files via upload

---
+++ src/install/final_nav/lib/python3.10/site-packages/final_nav/frontier_nav.py
@@ -0,0 +1,252 @@
+import sys
+import time
+
+from action_msgs.msg import GoalStatus
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav2_msgs.action import FollowWaypoints
+from nav2_msgs.srv import ManageLifecycleNodes
+from nav2_msgs.srv import GetCostmap
+from nav2_msgs.msg import Costmap
+from nav_msgs.msg  import OccupancyGrid
+from nav_msgs.msg import Odometry
+
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
+from rclpy.qos import QoSProfile
+
+from enum import Enum
+
+import numpy as np
+
+import math
+
+OCC_THRESHOLD = 10
+MIN_FRONTIER_LIST = 5
+
+class OccupancyGrid2d():
+    class CostValues(Enum):
+        FreeSpace = 0
+        InscribedInflated = 100
+        LethalObstacle = 100
+        NoInformation = -1
+
+    def __init__(self, map):
+        self.map = map
+
+    def getCost(self, mx, my):
+        return self.map.data[self.__getIndex(mx, my)]
+
+    def getSizeX(self):
+        return self.map.info.width
+
+    def getSizeY(self):
+        return self.map.info.height
+
+    def mapToWorld(self, mx, my):
+        wx = self.map.info.origin.position.x + (mx + 0.5) * self.map.info.resolution
+        wy = self.map.info.origin.position.y + (my + 0.5) * self.map.info.resolution
+
+        return (wx, wy)
+
+    def worldToMap(self, wx, wy):
+        if (wx < self.map.info.origin.position.x or wy < self.map.info.origin.position.y):
+            raise Exception("World coordinates out of bounds")
+
+        mx = int((wx - self.map.info.origin.position.x) / self.map.info.resolution)
+        my = int((wy - self.map.info.origin.position.y) / self.map.info.resolution)
+        
+        if  (my > self.map.info.height or mx > self.map.info.width):
+            raise Exception("Out of bounds")
+
+        return (mx, my)
+
+    def __getIndex(self, mx, my):
+        return my * self.map.info.width + mx
+    
+class FrontierCache():
+    cache = {}
+
+    def getPoint(self, x, y):
+        idx = self.__cantorHash(x, y)
+
+        if idx in self.cache:
+            return self.cache[idx]
+
+        self.cache[idx] = FrontierPoint(x, y)
+        return self.cache[idx]
+
+    def __cantorHash(self, x, y):
+        return (((x + y) * (x + y + 1)) / 2) + y
+
+    def clear(self):
+        self.cache = {}
+
+class FrontierPoint():
+    def __init__(self, x, y):
+        self.classification = 0
+        self.mapX = x
+        self.mapY = y
+
+def centroid(arr):
+    arr = np.array(arr)
+    length = arr.shape[0]
+    sum_x = np.sum(arr[:, 0])
+    sum_y = np.sum(arr[:, 1])
+    return sum_x/length, sum_y/length
+
+def findFree(mx, my, costmap):
+    fCache = FrontierCache()
+
+    bfs = [fCache.getPoint(mx, my)]
+
+    while len(bfs) > 0:
+        loc = bfs.pop(0)
+
+        if costmap.getCost(loc.mapX, loc.mapY) == OccupancyGrid2d.CostValues.FreeSpace.value:
+            return (loc.mapX, loc.mapY)
+
+        for n in getNeighbors(loc, costmap, fCache):
+            if n.classification & PointClassification.MapClosed.value == 0:
+                n.classification = n.classification | PointClassification.MapClosed.value
+                bfs.append(n)
+
+    return (mx, my)
+
+def getFrontier(pose, costmap, logger):
+    fCache = FrontierCache()
+
+    fCache.clear()
+
+    mx, my = costmap.worldToMap(pose.position.x, pose.position.y)
+
+    freePoint = findFree(mx, my, costmap)
+    start = fCache.getPoint(freePoint[0], freePoint[1])
+    start.classification = PointClassification.MapOpen.value
+    mapPointQueue = [start]
+
+    frontiers = []
+
+    while len(mapPointQueue) > 0:
+        p = mapPointQueue.pop(0)
+
+        if p.classification & PointClassification.MapClosed.value != 0:
+            continue
+
+        if isFrontierPoint(p, costmap, fCache):
+            p.classification = p.classification | PointClassification.FrontierOpen.value
+            frontierQueue = [p]
+            newFrontier = []
+
+            while len(frontierQueue) > 0:
+                q = frontierQueue.pop(0)
+
+                if q.classification & (PointClassification.MapClosed.value | PointClassification.FrontierClosed.value) != 0:
+                    continue
+
+                if isFrontierPoint(q, costmap, fCache):
+                    newFrontier.append(q)
+
+                    for w in getNeighbors(q, costmap, fCache):
+                        if w.classification & (PointClassification.FrontierOpen.value | PointClassification.FrontierClosed.value | PointClassification.MapClosed.value) == 0:
+                            w.classification = w.classification | PointClassification.FrontierOpen.value
+                            frontierQueue.append(w)
+
+                q.classification = q.classification | PointClassification.FrontierClosed.value
+
+            
+            newFrontierCords = []
+            for x in newFrontier:
+                x.classification = x.classification | PointClassification.MapClosed.value
+                newFrontierCords.append(costmap.mapToWorld(x.mapX, x.mapY))
+
+            if len(newFrontier) > MIN_FRONTIER_LIST:
+                frontiers.append(centroid(newFrontierCords))
+
+        for v in getNeighbors(p, costmap, fCache):
+            if v.classification & (PointClassification.MapOpen.value | PointClassification.MapClosed.value) == 0:
+                if any(costmap.getCost(x.mapX, x.mapY) == OccupancyGrid2d.CostValues.FreeSpace.value for x in getNeighbors(v, costmap, fCache)):
+                    v.classification = v.classification | PointClassification.MapOpen.value
+                    mapPointQueue.append(v)
+
+        p.classification = p.classification | PointClassification.MapClosed.value
+
+    return frontiers
+
+def getNeighbors(point, costmap, fCache):
+    neighbors = []
+
+    for x in range(point.mapX - 1, point.mapX + 2):
+        for y in range(point.mapY - 1, point.mapY + 2):
+            if (x > 0 and x < costmap.getSizeX() and y > 0 and y < costmap.getSizeY()):
+                neighbors.append(fCache.getPoint(x, y))
+
+    return neighbors
+
+def isFrontierPoint(point, costmap, fCache):
+    if costmap.getCost(point.mapX, point.mapY) != OccupancyGrid2d.CostValues.NoInformation.value:
+        return False
+
+    hasFree = False
+    for n in getNeighbors(point, costmap, fCache):
+        cost = costmap.getCost(n.mapX, n.mapY)
+
+        if cost > OCC_THRESHOLD:
+            return False
+
+        if cost == OccupancyGrid2d.CostValues.FreeSpace.value:
+            hasFree = True
+
+    return hasFree
+
+class PointClassification(Enum):
+    MapOpen = 1
+    MapClosed = 2
+    FrontierOpen = 4
+    FrontierClosed = 8
+
+class FrontierNav(Node):
+    def __init__(self):
+        super().__init__('frontier_nav')
+        self.currentPose = None
+
+        pose_qos = QoSProfile(
+          durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+          reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_RELIABLE,
+          history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+          depth=1)
+        
+        self.model_pose_sub = self.create_subscription(Odometry, '/odom', self.poseCallback, pose_qos)
+        self.costmapSub = self.create_subscription(OccupancyGrid(), '/map', self.occupancyGridCallback, pose_qos)
+        self.costmap = None
+        self.get_logger().info('Starting Navigation')
+
+    def occupancyGridCallback(self, msg):
+        self.costmap = OccupancyGrid2d(msg)
+
+    def move(self):
+        return
+    
+    def poseCallback(self, msg):
+        self.info_msg('Received amcl_pose')
+        self.currentPose = msg.pose.pose
+    
+    def info_msg(self, msg: str):
+        self.get_logger().info(msg)
+
+    def warn_msg(self, msg: str):
+        self.get_logger().warn(msg)
+
+    def error_msg(self, msg: str):
+        self.get_logger().error(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    auto_nav = FrontierNav()
+    rclpy.spin(auto_nav)
+    auto_nav.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()    
